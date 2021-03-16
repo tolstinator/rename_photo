d7d8fdcd266482534ea0c3ab7355a4787b4ce6c4
tolstinator <tolstonogov@mail.ru>
2021-03-16 11:41:27 +0700

    Refactoring code.

--- src/main/java/RenamePhoto.java
+++ src/main/java/RenamePhoto.java
@@ -11,31 +11,18 @@ public class RenamePhoto {
     public static final String DIRECTORY_PATH = System.getProperty("user.home") + "/Downloads/archive";
 
     public static void main(String[] args) throws IOException {
-        File directory = new File(DIRECTORY_PATH);
-        String year;
-        String month;
-        String day;
-        String hour;
-        String minute;
-        String second;
         String fileName;
-        LocalDateTime newFileDateTime;
-        for (File file : Objects.requireNonNull(directory.listFiles())) {
+        for (File file : Objects.requireNonNull(new File(DIRECTORY_PATH).listFiles())) {
             fileName = file.getName();
-            year = fileName.substring(4, 8);
-            month = fileName.substring(8, 10);
-            day = fileName.substring(10, 12);
-            hour = fileName.substring(13, 15);
-            minute = fileName.substring(15, 17);
-            second = fileName.substring(17, 19);
-            newFileDateTime = LocalDateTime.of(
-                    Integer.parseInt(year),
-                    Integer.parseInt(month),
-                    Integer.parseInt(day),
-                    Integer.parseInt(hour),
-                    Integer.parseInt(minute),
-                    Integer.parseInt(second));
-            Files.setLastModifiedTime(file.toPath(), FileTime.from(newFileDateTime.toInstant(ZoneOffset.UTC)));
+            Files.setLastModifiedTime(file.toPath(), FileTime.from(
+                    LocalDateTime.of(
+                            Integer.parseInt(fileName.substring(4, 8)),
+                            Integer.parseInt(fileName.substring(8, 10)),
+                            Integer.parseInt(fileName.substring(10, 12)),
+                            Integer.parseInt(fileName.substring(13, 15)),
+                            Integer.parseInt(fileName.substring(15, 17)),
+                            Integer.parseInt(fileName.substring(17, 19))).toInstant(ZoneOffset.UTC)
+            ));
         }
     }
 }
