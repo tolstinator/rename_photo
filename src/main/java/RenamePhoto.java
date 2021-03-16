2d2998b22b1e12944045e6a0990c27e74d7ce2eb
tolstonogovav <tolstonogovav@cifra-k.ru>
2021-03-16 11:35:23 +0700

    Fill code.

---
+++ src/main/java/RenamePhoto.java
@@ -0,0 +1,40 @@
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.attribute.FileTime;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+
+public class RenamePhoto {
+
+    public static final String DIRECTORY_PATH = System.getProperty("user.home") + "/Downloads/archive";
+
+    public static void main(String[] args) throws IOException {
+        File directory = new File(DIRECTORY_PATH);
+        String year;
+        String month;
+        String day;
+        String hour;
+        String minute;
+        String second;
+        String fileName;
+        LocalDateTime newFileDateTime;
+        for (File file : directory.listFiles()) {
+            fileName = file.getName();
+            year = fileName.substring(4, 8);
+            month = fileName.substring(8, 10);
+            day = fileName.substring(10, 12);
+            hour = fileName.substring(13, 15);
+            minute = fileName.substring(15, 17);
+            second = fileName.substring(17, 19);
+            newFileDateTime = LocalDateTime.of(
+                    Integer.parseInt(year),
+                    Integer.parseInt(month),
+                    Integer.parseInt(day),
+                    Integer.parseInt(hour),
+                    Integer.parseInt(minute),
+                    Integer.parseInt(second));
+            Files.setLastModifiedTime(file.toPath(), FileTime.from(newFileDateTime.toInstant(ZoneOffset.UTC)));
+        }
+    }
+}
